2a0c0ae478ebf29c81653c61970433ddeac95d95
Kanwaljit <kawalzaff7@gmail.com>
2026-07-10 00:56:58 -0700

    Added Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,3 +107,15 @@ export default function RootLayout({
     </html>
   );
 }
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);
+      t.async=1;
+      t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xk5m7gohez");
+  `}
+</Script>
